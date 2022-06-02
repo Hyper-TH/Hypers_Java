d8a2b5aa8ece3a28fbcab13a0354d7de690bb5e1
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-02 08:36:51 +0100

    Added Lesson9

---
+++ methods_objects/src/Lesson9.java
@@ -0,0 +1,59 @@
+/*
+    https://www.youtube.com/watch?v=9BZ8s79jkP0
+ */
+
+package methods_objects.src;
+
+public class Lesson9
+{
+    public static void main(String[] args)
+    {
+        Aircraft cessna172 = new Aircraft();
+        Aircraft piperSaratoga;
+        piperSaratoga = new Aircraft();
+
+        cessna172.passengers = 4;
+        cessna172.cruiseSpeed = 140;
+        cessna172.fuelCapacity = 56.5;
+        cessna172.fuelBurnRate = 9.5;
+
+        piperSaratoga.passengers = 6;
+        piperSaratoga.cruiseSpeed = 201;
+        piperSaratoga.fuelCapacity = 102.5;
+        piperSaratoga.fuelBurnRate = 20.5;
+
+        System.out.println("For Cessna172 to fly for 2.5 hours, it takes "
+                + cessna172.fuelNeeded(2.5)
+                + " gallons of fuel");
+
+        System.out.println("For Piper to fly for 4.2 hours, it takes "
+                + piperSaratoga.fuelNeeded(4.2)
+                + " gallons of fuel");
+
+    } // end main()
+
+} // end Lesson9 class
+
+class Aircraft
+{
+    int passengers;         // number of people
+    int cruiseSpeed;        // miles per hour
+    double fuelCapacity;    // gallons
+    double fuelBurnRate;    // gallons per hour
+
+    // Method to calculate aircraft endurance
+    double calculateEndurance()
+    {
+        double endurance;
+
+        endurance = fuelCapacity / fuelBurnRate;
+
+        return endurance;
+    }  // end calculateEndurance();
+
+    double fuelNeeded(double time)
+    {
+        return fuelBurnRate * time;
+    } // end fuelNeeded()
+
+} // end Aircraft class
