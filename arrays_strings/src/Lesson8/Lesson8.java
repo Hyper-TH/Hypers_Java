4365abfea42878291bf0dc9914b01693416df2d3
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-21 23:28:39 +0100

    Added Lesson8

---
+++ arrays_strings/src/Lesson8/Lesson8.java
@@ -0,0 +1,24 @@
+/*
+    https://www.youtube.com/watch?v=uvT_vAuy5bw&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=15
+
+    Program to demonstrate concatenating strings
+
+    Implemented in IntelliJ
+    Pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson8;
+
+public class Lesson8
+{
+    public static void main(String[] args)
+    {
+        String lastName = "GB";
+        String firstName = "Hyper";
+        String middleName = "Max";
+
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println(fullName);
+    } // end main()
+
+} // end Lesson8 Class
