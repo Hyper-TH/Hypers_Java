9ad5aa4d76e2f7e082304c3fa17b0c98c04cb045
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-22 23:19:30 +0100

    Added Lesson9

---
+++ arrays_strings/src/Lesson9/Lesson9.java
@@ -0,0 +1,33 @@
+/*
+    https://www.youtube.com/watch?v=vPlbqJ6taK0&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=17
+
+    Program to demonstrate obtaining string length in java
+
+    Implemented and pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson9;
+
+public class Lesson9
+{
+    public static void main(String[] args)
+    {
+        String car1 = "Ford";
+        String car2 = "Chevy";
+        String car3 = "Mitsubishi";
+        String str = "I love cars, they're a great deal.";
+
+        System.out.println(car1);
+        System.out.println(car1.length() + "\n");
+
+        System.out.println(car2);
+        System.out.println(car2.length() + "\n");
+
+        System.out.println(car3);
+        System.out.println(car3.length() + "\n");
+
+        System.out.println(str);
+        System.out.println(str.length() + "\n");
+
+    } // end main()
+
+} // end Lesson9 class
