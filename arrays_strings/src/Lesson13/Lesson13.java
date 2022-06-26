21b8b1489901e7f3a477fbac6e84a161cee82b2b
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-26 22:14:29 +0100

    Added Lesson13

---
+++ arrays_strings/src/Lesson13/Lesson13.java
@@ -0,0 +1,21 @@
+/*
+    https://www.youtube.com/watch?v=udwBI1r7BDo&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=25
+
+    Program to demonstrate searching a string from a substring
+
+    Implemented and pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson13;
+
+public class Lesson13
+{
+
+    public static void main(String[] args)
+    {
+        String hyper = "I love going to the shop";
+
+        System.out.println(hyper);
+        System.out.println(hyper.indexOf("ing tkdd"));
+    } // end main()
+
+} // end Lesson13 class
