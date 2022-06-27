efa7b1aa673714c04878ed3e2c9ecfd4aaa051a5
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-27 19:52:00 +0100

    Added Lesson15

---
+++ arrays_strings/src/Lesson15/Lesson15.java
@@ -0,0 +1,21 @@
+/*
+    https://www.youtube.com/watch?v=1PpE8YGvtUg&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=29
+
+    Program to demonstrate searching a string from the end for a substring
+
+    Implemented and pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson15;
+
+public class Lesson15
+{
+    public static void main(String[] args)
+    {
+        String hyper = "I love to go to the shops.";
+
+        System.out.println(hyper);
+        System.out.println(hyper.lastIndexOf("go")); // g starts at index 10
+
+    } // end main()
+
+} // end Lesson15 class
