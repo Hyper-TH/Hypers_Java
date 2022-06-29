db53ea4dad64551bde819ff4b8b94a719a45a4c4
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-29 22:45:15 +0100

    Added Lesson17

---
+++ arrays_strings/src/Lesson17/Lesson17.java
@@ -0,0 +1,24 @@
+/*
+    https://www.youtube.com/watch?v=dwm_BR6K_po&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=33
+
+    Program to demonstrate changing a string to lower / upper case
+
+    Implemented and pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson17;
+
+public class Lesson17
+{
+    public static void main(String[] args)
+    {
+        String str1  = "this is uppercase";
+        String str2 = "THIS IS LOWERCASE";
+
+        System.out.println(str1.toUpperCase());
+        System.out.println(str2.toLowerCase());
+
+        System.out.println(str1);
+        System.out.println(str2);
+    } // end main()
+
+} // end Lesson17 class
