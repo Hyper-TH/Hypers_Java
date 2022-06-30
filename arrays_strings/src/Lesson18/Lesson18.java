471c3a162783ad6889e5fe7df215f5d0a599eb0f
Hyper-TH <twila.shayne.habab@outlook.com>
2022-06-30 23:07:49 +0100

    Added Lesson18

---
+++ arrays_strings/src/Lesson18/Lesson18.java
@@ -0,0 +1,22 @@
+/*
+    https://www.youtube.com/watch?v=A4K-uKY8N-A&list=PLnVYEpTNGNtU6TSAVSv_Kw7pyBbYrTFQV&index=35
+
+    Program to demonstrate replacing chars in a string
+    Implemented and pushed in IntelliJ
+ */
+package arrays_strings.src.Lesson18;
+
+public class Lesson18
+{
+    public static void main(String[] args)
+    {
+        String str = "I love cheese";
+
+        System.out.println(str);
+
+        str = str.replace("cheese", "fruits");
+
+        System.out.println(str);
+    } // end main()
+
+} // end Lesson18 class
